0fad610f299713177d49cc5d9ade58dff6a76856
Thebza36 <tbkmaboee@gmail.com>
2026-09-21 08:10:02 -0700

    Fix User model import case

--- backend/controllers/analyticsController.js
+++ backend/controllers/analyticsController.js
@@ -1,5 +1,5 @@
 const WaterSale = require("../models/WaterSale");
-const User = require("../models/User");
+const User = require("../models/user");
 
 // =====================================================
 // TOTAL REVENUE
